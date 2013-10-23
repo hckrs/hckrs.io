7d43cdcb769d70db503b6fc08bdbd453ac12c428
Toon van Ramshorst <ramshorst@gmail.com>
2013-10-23 20:19:26 +0200

    Originele site

---
+++ website/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hackers Welcome</title>
+	<meta name="title" content="Hackers Welcome" />
+	<meta name="description" content="Community of hackers [dev, design & hardware] in your city." />
+
+	<meta content="http://www.HackersWelcome.com" property="og:url" />
+	<!--<meta content="" property="og:image" />
+	<link rel="shortcut icon" id="favicon" href="favicon.png"> -->
+	<meta name="author" content="Toon van Ramshorst &amp; Jarno Le Conté">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script type="text/javascript" src="js/typewritter.js"></script>
+  <link href='style.css' rel='stylesheet' type='text/css'>
+	<script>
+	  $(document).ready(function(){
+      // some action here!
+		});
+	</script>
+</head>
+<body>
+  <div class="wrapper">
+	  <div class="main">
+      <div class="header">
+        <a href="http://www.hackerswelcome.com"><img src="img/hackers-welcome.png" alt="Hackers Welcome" class="logo"></a>
+        <a href="#"><img src="img/facebook-connect.png" alt="Facebook Connect" class="facebook-connect"></a>
+      </div>
+      <div class="content">
+        <H1>Lyon has <b>4</b> hackers yet.<br>Are you a <b><span id="target"></span><span id="cursor"></span></b> hacker ?</H1>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
