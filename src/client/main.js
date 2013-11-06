3bdf6a6e887514da9cbee60fc604b83409d9fe41
Toon van Ramshorst <ramshorst@gmail.com>
2013-11-06 19:02:01 +0100

    Typewritter text changes

--- src/client/main.js
+++ src/client/main.js
@@ -8,11 +8,10 @@ Template.frontpage.helpers({
 
 // typer text on frontpage
 Template.frontpage.rendered = function() {
-  var texts = ['node.js','Arduino','design','meteor','UX','hardware','life','backbone.js','mobile'];
+  var texts = ['node.js','arduino','design','game','UX','hardware','life','backbone.js','mobile'];
   $('#target').teletype({ text: texts }); 
   $('#cursor').teletype({ text: [' ', ' '], delay: 0, pause: 500 });
 }
-
 
 
 /* HACKERS list */
@@ -21,15 +20,3 @@ Template.frontpage.rendered = function() {
 Template.hackers.helpers({
   "hackers": function() { return Meteor.users.find({}, {reactive: false}).fetch(); }
 });
-
-
-
-
-
-
-
-
-
-
-
-
