9aeb7611830d4fb5812aab2dfada6a3a80974fad
Jarno Le Conté <jarno.leconte@me.com>
2013-11-26 20:48:43 +0100

    Input validation server-side
    Valideer input nu ook inhoudelijk op server-side.
    - Strings hebben maximum lengte. Hiermee wordt voorkomen dat iemand
    oneindig veel data in de database kan stoppen.
    - E-mailvalidatie door middel van regular expression.

--- src/helpers.js
+++ src/helpers.js
@@ -115,15 +115,58 @@ if (Meteor.isClient) {
 
 }
 
-if (Meteor.isServer) {
 
-  /* MATCH helpers */
-  Match.In = function(allowedValues) {
-    return Match.Where(function(value) {
-      return _.contains(allowedValues, value);
-    });
-  }
-  Match.AllIn = function(allowedValues) {
-    return [ Match.In(allowedValues) ];
-  }
+
+/* MATCH helpers */
+
+// pass in an array of allowed elements
+// when matching a string it must be present in the allowed array
+Match.In = function(allowedValues) {
+  return Match.Where(function(value) {
+    return _.contains(allowedValues, value);
+  });
 }
+
+// pass in an array of allowed elements
+// when matching an array, all elements must be in the allowed array
+Match.AllIn = function(allowedValues) {
+  return [ Match.In(allowedValues) ];
+}
+
+// match e-mailaddress
+Match.Email = Match.Where(function(email) {
+  return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+});
+
+// match empty or the given pattern
+Match.EmptyOr = function(pattern) {
+  return Match.OneOf(Match.Empty, pattern);
+}
+
+// match empty value (string, array, object, null, undefined, ...?)
+Match.Empty = Match.Where(function(value) {
+  return _.isEmpty(value);
+});
+
+// maximum character length
+Match.Max = function(max, pattern) {
+  return Match.Where(function(value) {
+    var valid = value.length <= max;
+    return pattern ? valid && Match.test(value, pattern) : valid;
+  });
+}
+
+// minimum character length
+Match.Min = function(min, pattern) {
+  return Match.Where(function(value) {
+    var valid = value.length >= min;
+    return pattern ? valid && Match.test(value, pattern) : valid;
+  });
+}
+
+// minimum and maximum
+Match.MinMax = function(min, max, pattern) {
+  return Match.Min(min, Match.Max(max, pattern));
+}  
+
+
