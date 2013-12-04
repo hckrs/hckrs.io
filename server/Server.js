a93be6b1368a978927091856f403cdffbdcf9efc
Jarno Le Conté <jarno.leconte@me.com>
2013-12-04 15:38:24 +0100

    Prevent Heroku app from sleeping
    Heroku normally set an app in sleeping mode when there is no activity.
    The disadventage for our app is that the next request takes more time
    to load. This is a bad experience for visitors. We prevent from going
    to sleep by making sure that there is activity within every minute.

--- server/Server.js
+++ server/Server.js
@@ -3,9 +3,23 @@
 Meteor.startup(function() {
   
   /* server startup */
-  // ...
+  
+  _keepAlive(); // prevent Heroku app from going to sleeping mode
   
 });
+
+
+
+// prevent app from sleeping (in Heroku's dyno field)
+// make sure there is activity every minute
+var _keepAlive = function() {
+  var startupDate = new Date();
+  var signal = function() { return true; }; 
+  var logging = function() { log("[Server Uptime]:" + moment(startupDate).fromNow(true)); }
+  Meteor.setTimeout(logging, 1000);
+  Meteor.setInterval(signal, 5000);
+  Meteor.setInterval(logging, 55000);
+}
 
 
 
