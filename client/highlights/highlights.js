05b84bcdbfcdd41bbd94fc30ac00259afaad10ee
Toon van Ramshorst <ramshorst@gmail.com>
2014-01-12 17:42:24 +0100

    Highlights styling
    — move images to img/highlights/
    — css styling of highlights (not fished)

--- client/highlights/highlights.js
+++ client/highlights/highlights.js
@@ -34,7 +34,7 @@ Template.highlights.rendered = function() {
   this.onePageScroll = $("#highlights").onepage_scroll({
     sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
     easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-    animationTime: 1000, // AnimationTime let you define how long each section takes to animate
+    animationTime: 900, // AnimationTime let you define how long each section takes to animate
     pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
     updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
     beforeMove: function(index) {}, // This option accepts a callback function. The function will be called before the page moves.
