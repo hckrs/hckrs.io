84c8e3098bb0fe5a1bb54b049840365a3fa38c1e
Jarno Le Conté <jarno.leconte@me.com>
2014-01-26 22:01:12 +0100

    bugfix: highlights scrollbar
    No scrollbar on highlights page. By wrapping the onePageScroll into a
    container.

--- client/highlights/highlights.js
+++ client/highlights/highlights.js
@@ -31,7 +31,7 @@ Template.highlights.helpers({
 Template.highlights.rendered = function() {
   /* jquery stuff */
 
-  this.onePageScroll = $("#highlights").onepage_scroll({
+  this.onePageScroll = $("#onePageScroll").onepage_scroll({
     sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
     easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
     animationTime: 900, // AnimationTime let you define how long each section takes to animate
