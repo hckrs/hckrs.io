80de227b81d3c5c9226623f99fc8fc916fa40172
Jarno Le Conté <jarno.leconte@me.com>
2014-06-09 22:43:03 +0200

    remove unused code

--- client/lib/_startup.js
+++ client/lib/_startup.js
@@ -4,11 +4,6 @@
 
 Meteor.startup(function() {
 
-  // keep track of the login session
-  Session.set('currentLoginState', 'loggedOut');
-  Session.set('subscriptionsReady', false);
-  Session.set('userSubscriptionsReady', false);
-
   // language
   // XXX use English with Europe formatting
   // in the future we probably use navigator.language || navigator.userLanguage; 
