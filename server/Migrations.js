a1cb976f7bfd466be9423600973b78f6100f2a7a
Jarno Le Conté <jarno.leconte@me.com>
2014-07-29 11:50:07 +0200

    Create migration for new ambassador fields

--- server/Migrations.js
+++ server/Migrations.js
@@ -15,23 +15,18 @@ var migrations = [
       
       // change (allowAccess & isInvited) to isAccessDenied
       Meteor.users.update({$or: [{allowAccess: {$ne: true}}, {isInvited: {$ne: true}}]}, {$set: {isAccessDenied: true, isHidden: true}}, {multi: true});
-      log('1/5');
 
       // unset allowAccess
       Meteor.users.update({}, {$unset: {allowAccess: true}}, {multi: true});
-      log('2/5');
 
       // unset isInvited
       Meteor.users.update({}, {$unset: {isInvited: true}}, {multi: true});  
-      log('3/5');
       
       // hide deleted accounts
       Meteor.users.update({isDeleted: true}, {$set: {isHidden: true}}, {multi: true});      
-      log('4/5');
 
       // make users with localRank===1 mayor
       Meteor.users.update({localRank: 1}, {$set: {isMayor: true}}, {multi: true});      
-      log('5/5');
 
       // done
       callback();
@@ -44,7 +39,6 @@ var migrations = [
       
       // change (allowAccess & isInvited) to isAccessDenied
       Meteor.users.update({isAccessDenied: true}, {$set: {isIncompleteProfile: true}}, {multi: true});
-      log('1/1');
 
       // done
       callback();
@@ -62,7 +56,6 @@ var migrations = [
         if (!isInvited)
           Meteor.users.update(user._id, {$set: {isUninvited: true, isAccessDenied: true, isHidden: true}});
       });
-      log('1/1');
 
       // done
       callback();
@@ -88,8 +81,35 @@ var migrations = [
     }
   },
 
-];
+  { // 29 juli 2014
+    name: "Simple Schema",
+    task: function(callback) {
 
+      // add field 'currentCity' to all users
+      Users.find().forEach(function(user) {
+        Users.update(user._id, {$set: {currentCity: user.city}}, {validate: false});
+      });
+
+      // rename field signedupAt to 'createdAt' for all invitations
+      Invitations.find().forEach(function(invitation) {
+        Invitations.update(invitation._id, {$set: {createdAt: invitation.signedupAt}, $unset: {signedupAt: true}}, {validate: false})
+      });
+
+      // add global attribute to highlights and gifts
+      Highlights.update({}, {$set: {global: false}}, {multi: true, validate: false});
+      Gifts.update({}, {$set: {global: false}}, {multi: true, validate: false});
+      
+      // add field 'createdAt' to migrations
+      Migrations.find().forEach(function(migration) {
+        Migrations.update(migration._id, {$set: {createdAt: migration.processedAt}}, {validate: false});
+      });
+
+      // done
+      callback();
+    }
+  }
+
+];
 
 
 
@@ -101,7 +121,7 @@ var runMigration = function(migration, callback) {
 
   // start
   Migrations.insert({name: migration.name, status: 'inProgress', processedAt: new Date()});
-  log('Running migration:', migration.name);
+  log('TASK:', migration.name);
 
   // process
   migration.task(function(err) {
@@ -111,7 +131,6 @@ var runMigration = function(migration, callback) {
     
     // finishing
     Migrations.update({name: migration.name}, {$set: {status: 'done', processedAt: new Date()}});
-    log('Finishing migration:', migration.name);
 
     // successful migrated
     callback();
@@ -122,23 +141,36 @@ var runMigration = function(migration, callback) {
 
 // run required migrations on startup
 Meteor.startup(function() {
-  
-  // loop through migration
-  async.eachSeries(migrations, function(migration, callback) {
-      
-    // check if migration is already processed
+
+  var allDone = _.every(migrations, function(migration) {
     var processed = Migrations.findOne({name: migration.name});
-    
-    if (!processed) { //run
-      runMigration(migration, callback); 
-    } else if (processed.status === 'done') { //already done
-      callback(); 
-    } else { //error
-      callback("Migration '" + migration.name + "' not finished previous time.")
-    }
-
-  }, function(err) {
-    if (err) log("[Migration Error]", err);
+    return processed && processed.status === 'done';
   });
+  
+  if (!allDone) {
+  
+    log("BEGIN MIGRATIONS")  
 
+    // loop through migration
+    async.eachSeries(migrations, function(migration, callback) {
+        
+      // check if migration is already processed
+      var processed = Migrations.findOne({name: migration.name});
+      
+      if (!processed) { //run
+        runMigration(migration, callback); 
+      } else if (processed.status === 'done') { //already done
+        callback(); 
+      } else { //error
+        callback("Migration '" + migration.name + "' not finished previous time.")
+      }
+
+    }, function(err) {
+      if (err) 
+        log("[Migration Error]", err);
+      else
+        log("END MIGRATIONS")
+    });
+
+  }
 });
