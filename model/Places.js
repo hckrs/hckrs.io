983350c3ef3c41fc84f88f4361aed0444513ac9e
Jarno Le Conté <jarno.leconte@me.com>
2014-07-30 12:43:56 +0200

    fix coworking icon

--- model/Places.js
+++ model/Places.js
@@ -4,7 +4,7 @@ PLACE_TYPE_OPTIONS = [
   {value: "fablab", label: "FabLab"},
   {value: "wifi-cafe", label: "Wi-Fi Cafe"},
   {value: "meetup", label: "Meetup"},
-  {value: "corworking", label: "Coworking Space"},
+  {value: "coworking", label: "Coworking Space"},
   {value: "hackerspace", label: "Hacker Space"},
   {value: "incubator", label: "Incubator"},
 ]
