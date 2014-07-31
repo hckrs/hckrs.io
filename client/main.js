473cb5b14becaa9e7f07376b462801a73a6f1ac2
Jarno Le Conté <jarno.leconte@me.com>
2014-07-31 14:49:53 +0200

    Introduce counters again

--- client/main.js
+++ client/main.js
@@ -83,6 +83,27 @@ Template.main.rendered = function() {
 }
 
 
+// precalculate city user-counters
+var _cityCount, _cityVisibleCount; 
+
+var _calculateCount = function() {
+  console.log('recalc')
+  var users = Users.find({}, {fields: {city: true}, reactive: false}).fetch();
+  var visibleUsers = Users.find({isHidden: {$ne: true}}, {fields: {city: true}, reactive: false}).fetch();
+  _cityCount = _.countBy(users, 'city');
+  _cityVisibleCount = _.countBy(visibleUsers, 'city');
+}
+
+var getCityCount = function() {
+  if (!_cityCount) _calculateCount();
+  return _cityCount;
+}
+var getCityVisibleCount = function() {
+  if (!_cityVisibleCount) _calculateCount();
+  return _cityVisibleCount;
+}
+
+
 
 /* CITY SELECT */
 
@@ -91,18 +112,13 @@ Template.citySelect.helpers({
     
     var createCityEntry = _.identity;
 
-    // if (hasAdminPermission()) {
-    //   var users = Users.find({}, {fields: {city: true}, reactive: false}).fetch();
-    //   var visibleUsers = Users.find({isHidden: {$ne: true}}, {fields: {city: true}, reactive: false}).fetch();
-    //   var cityCount = _.countBy(users, 'city');
-    //   var cityVisibleCount = _.countBy(visibleUsers, 'city');
-    
-    //   createCityEntry = function(city) {
-    //     city.usersCount = cityCount[city.key];
-    //     city.visibleUsersCount = cityVisibleCount[city.key];
-    //     return city;
-    //   }
-    // }
+    if (hasAdminPermission()) {
+      createCityEntry = function(city) {
+        city.usersCount = getCityCount()[city.key];
+        city.visibleUsersCount = getCityVisibleCount()[city.key];
+        return city;
+      }
+    }
     
     var createCountryEntry = function(cities, countryCode) {
       return { 
