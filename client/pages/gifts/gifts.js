bd5dac105fc68fdbad272a5570cb204f2038b364
Jarno Le Conté <jarno.leconte@me.com>
2014-08-01 12:37:25 +0200

    Make Gift-editor same as Highlight-editor and focus fields.

--- client/pages/gifts/gifts.js
+++ client/pages/gifts/gifts.js
@@ -30,6 +30,21 @@ GiftsController = DefaultController.extend({
 
 
 
+// helper functions
+
+var mode = function() {
+  if (Session.get('showGiftAdd')) return 'insert';
+  if (Session.get('giftIdInEditMode')) return 'update';
+  return false;
+}
+
+var close = function() {
+  Session.set('giftIdInEditMode', null);     
+  Session.set('showGiftAdd', false);
+}
+
+
+
 Template.gifts.helpers({
   'isEmpty': function() {
     return Gifts.find(selector()).count() === 0;
@@ -37,29 +52,32 @@ Template.gifts.helpers({
   'gifts': function() {
     return GiftsSorted();
   },
-  'editMode': function() {
-    return Session.equals('giftIdInEditMode', this._id);
-  }
 });
 
 Template.gift.helpers({
   'codeType': function() {
     return /^http/.test(this.code) ? 'url' : 'code';
   },
-  hiddenGift: function() {
+  'hiddenGift': function() {
     var city = Session.get('currentCity');
     return !!Gifts.findOne({_id: this._id, hiddenIn: city});
   }
-})
+});
 
-Template.giftAdd.helpers({
-  'show': function() {
-    return Session.equals('showGiftAdd', true) ? '' : 'hide';
-  },
-  'hidden': function() {
-    return !Session.equals('showGiftAdd', true);
+Template.giftEditor.helpers({
+  'mode': function() {
+    return mode();
   }
-})
+});
+
+Template.giftEditorForm.helpers({
+  'mode': function() {
+    return mode();
+  },
+  'selectedDoc': function() {
+    return Gifts.findOne(Session.get('giftIdInEditMode')) || null;
+  }
+});
 
 
 /* events */
@@ -67,43 +85,53 @@ Template.giftAdd.helpers({
 Template.gift.events({
   "click [action='edit']": function(evt) {
     var giftId = $(evt.currentTarget).data('gift')
+    close();
     Session.set('giftIdInEditMode', giftId);
-
-  },
-  "click [action='remove']": function(evt) {
-    var giftId = $(evt.currentTarget).data('gift')
-    Gifts.remove(giftId);
   },
   "click [action='visibility']": function(evt) {
     var action = $(evt.currentTarget).attr('toggle') === 'off' ? '$addToSet' : '$pull';
     var selectedId = $(evt.currentTarget).data('id');
     var city = Session.get('currentCity');
     Gifts.update(selectedId, _.object([action], [{hiddenIn: city}]));
+  }
+});
+
+Template.giftEditor.events({
+  "click [action='add']": function() {
+    close();
+    Session.set('showGiftAdd', true);
+  }
+});
+
+Template.giftEditorForm.events({
+  "click [action='cancel']": function() {
+    close();
+  },
+  "click [action='remove']": function(evt) {
+    var giftId = $(evt.currentTarget).data('gift')
+    Gifts.remove(giftId);
+    close();
   },
 });
 
-Template.giftEdit.events({
-  "click [action='cancel']": function() {
-    Session.set('giftIdInEditMode', null);    
+Template.giftEditorForm.rendered = function() {
+  var $input = this.$('input:first');
+  Deps.autorun(function(c) {
+    mode(); /* reactive depend */
+    $input.blur();
+    setTimeout(function() {
+      $input.focus();
+    }, 200);
+  });
+}
+
+
+AutoForm.addHooks('giftEditorForm', {
+  onSuccess: function(operation) {
+    close();
   }
 });
 
-Template.giftAdd.events({
-  "click [action='add']": function() {
-    Session.set('showGiftAdd', true)
-  }
-})
-
-AutoForm.addHooks('giftEditForm', {
-  after: {
-    update: function() {
-      Session.set('giftIdInEditMode', null);    
-    },
-    insert: function() {
-      Session.set('showGiftAdd', false);
-    }
-  }
-});
 
 
 
