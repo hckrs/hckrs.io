79b7aacd32fe21907600051b7fe2ef17c166f3cf
Jarno Le Conté <jarno.leconte@me.com>
2015-11-20 14:41:06 +0100

    Use Google API key.

--- client/pages/frontpage/locationFinder.js
+++ client/pages/frontpage/locationFinder.js
@@ -3,53 +3,71 @@ var state = new State('locationFinder', {
   "suggestions": []
 })
 
+
+
 Template.frontpageLocationFinder.rendered = function() {
   var input = this.$("#locationAutoComplete").get(0)
     , autocomplete;
 
-  var options = {
-    types: ['(cities)']
-  };
+  var initializeAutoComplete = function() {
 
-  // setup autocomplete
-  autocomplete = new google.maps.places.Autocomplete(input, options);
+    var autocompleteOptions = {
+      types: ['(cities)']
+    };
 
-  // event handlers
-  var placeChanged = function() {
-    var place = autocomplete.getPlace();
-    var latlng = {
-      lat: place.geometry.location.lat(),
-      lng: place.geometry.location.lng()
+    // setup autocomplete
+    autocomplete = new google.maps.places.Autocomplete(input, autocompleteOptions);
+
+    // event handlers
+    var placeChanged = function() {
+      var place = autocomplete.getPlace();
+      var latlng = {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng()
+      }
+      createSuggestions(latlng);
     }
-    createSuggestions(latlng);
-  }
 
-  var createSuggestions = function(latlng) {
+    var createSuggestions = function(latlng) {
 
-    // get cities nearby the searched location
-    var cities = getNearbyCities(latlng);
+      // get cities nearby the searched location
+      var cities = getNearbyCities(latlng);
 
-    // best match
-    var best = _.first(cities);
+      // best match
+      var best = _.first(cities);
 
-    // If best match is very close (<1km) we choose that city directly
-    // otherwise we suggest 3 cities nearby
-    if (Geo.getDistanceFromLatLon(latlng.lat, latlng.lng, best.latitude, best.longitude) < 1)
-      goToCity(best.key);
-    else
-      state.set('suggestions', cities);
-  }
-
-  var getNearbyCities = function(latlng) {
-    var dist = function(city) {
-      return Geo.getDistanceFromLatLon(latlng.lat, latlng.lng, city.latitude, city.longitude);
+      // If best match is very close (<1km) we choose that city directly
+      // otherwise we suggest 3 cities nearby
+      if (Geo.getDistanceFromLatLon(latlng.lat, latlng.lng, best.latitude, best.longitude) < 1)
+        goToCity(best.key);
+      else
+        state.set('suggestions', cities);
     }
-    return _.chain(City.cities()).sortBy(dist).first(3).value();
+
+    var getNearbyCities = function(latlng) {
+      var dist = function(city) {
+        return Geo.getDistanceFromLatLon(latlng.lat, latlng.lng, city.latitude, city.longitude);
+      }
+      return _.chain(City.cities()).sortBy(dist).first(3).value();
+    }
+
+    // setup events
+    google.maps.event.addListener(autocomplete, 'place_changed', placeChanged);
   }
 
-  // setup events
-  google.maps.event.addListener(autocomplete, 'place_changed', placeChanged);
+  window.gMapsCallback = function() {
+    initializeAutoComplete();
+  }
 
+  var loadGoogleMaps = function() {
+    var apiKey = Settings['environment'] === 'local' ? "" : Settings['googleMapsAPI'].key;
+    var script_tag = document.createElement('script');
+    script_tag.setAttribute("type","text/javascript");
+    script_tag.setAttribute("src","http://maps.google.com/maps/api/js?libraries=places&callback=gMapsCallback&key=" + apiKey);
+    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+  }
+
+  loadGoogleMaps();
 }
 
 Template.frontpageLocationFinder.helpers({
